44f8287162630d86fb3e81267f5ffd3ad1ec1f9f
m1ke <git@m1ke.me>
2015-12-29 20:32:45 +0000

    Loading input file

--- five.java
+++ five.java
@@ -7,6 +7,7 @@ A nice string is one with all of the following properties:
 It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
 It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
 It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+
 For example:
 
 ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...), and none of the disallowed substrings.
@@ -14,15 +15,34 @@ aaa is nice because it has at least three vowels and a double letter, even thoug
 jchzalrnumimnmhp is naughty because it has no double letter.
 haegwjzuvuyypxyu is naughty because it contains the string xy.
 dvszwmarrgswjxmb is naughty because it contains only one vowel.
+
 How many strings are nice?
 
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class five {
 
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+    public static void main(String[] args) throws IOException {
+        String[] lines = readLines("5-input");
+        System.out.println(lines.length);
     }
+
+     public static String[] readLines(String filename) throws IOException {
+     	 FileReader fileReader = new FileReader(filename);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        List<String> lines = new ArrayList<String>();
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+            lines.add(line);
+        }
+        bufferedReader.close();
+        return lines.toArray(new String[lines.size()]);
+     }
 
 }
