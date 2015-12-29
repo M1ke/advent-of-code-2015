069c8e42b43d5c8a753dff75579e7a18cbd74f41
m1ke <git@m1ke.me>
2015-12-29 17:26:01 +0000

    Part 2 working in go

--- 3.go
+++ 3.go
@@ -44,12 +44,20 @@ func main() {
     var coords [2]int
     coords[0] = 0
     coords[1] = 0
+    var coords_santa = coords
+    var coords_robot = coords
     history := make([]string, 0)
     history = append(history, "0,0")
 
     var coord string
+    var which = 0
 
     for _,move := range moves {
+    	if which==1 {
+    		coords = coords_robot
+    	} else {
+    		coords = coords_santa
+    	}
     	fmt.Println(string(move))
     	switch rune(move) {
     	case 'v':
@@ -61,12 +69,34 @@ func main() {
     	case '^':
     		coords[1] += 1
     	}
+    	if which==1 {
+    		coords_robot = coords
+    	} else {
+    		coords_santa = coords
+    	}
     	coord = strconv.Itoa(coords[0]) + "," + strconv.Itoa(coords[1])
     	fmt.Println(coord)
     	if (!stringInSlice(coord, history)){
     		history = append(history, coord)
     	}
+    	which = 1 - which
     }
 
     fmt.Println(len(history))
 }
+
+/*
+
+The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents with him.
+
+Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns moving based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
+
+This year, how many houses receive at least one present?
+
+For example:
+
+^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
+^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
+
+*/
